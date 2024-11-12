13c67aa290e971399787047a14833267f1d10f9d
Jun <junhuito5771@gmail.com>
2024-11-13 00:35:33 +0800

    test

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -44,3 +44,58 @@ console.log('Workload info...', process.env.WORKLOAD_INFO)
 // }
 
 // provisionStepFunctions(commonConfigs, environmentConfigs, stepFunctionDependencies);
+
+
+
+
+
+const accounts = [
+  {
+    name: "AccountA",
+    region: "us-east-1" as aws.Region,
+    awsAccountId: "<ACCOUNT_A_ID>",
+    s3Bucket: "pulumi-backend-account-a",
+    stackName: "stack-a"
+  },
+  {
+    name: "AccountB",
+    region: "us-west-2" as aws.Region,
+    awsAccountId: "<ACCOUNT_B_ID>",
+    s3Bucket: "pulumi-backend-account-b",
+    stackName: "stack-b"
+  },
+  // Add more accounts as needed
+];
+
+
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+interface TenantConfig {
+    account: string;
+    region: aws.Region;
+    abbr: string;
+}
+
+// Function to retrieve tenant configurations from SSM in the parent account
+async function getTenantConfigs(): Promise<Array<TenantConfig>> {
+    const ssmConfig = aws.ssm.Parameter.get("tenant-configs", "/configs/deployment/sandbox/434343955077-ap-southeast-1").value;
+    
+    return new Promise((resolve) => ssmConfig.apply((config) => resolve(JSON.parse(config)))) 
+}
+
+pulumi.runtime.runInPulumiStack(async () => {
+    const tenantConfigs = await getTenantConfigs();
+
+    tenantConfigs.forEach(config => {
+        const provider = new aws.Provider(`tenant-provider-${config.account}`, {
+            region: config.region,
+            assumeRole: {
+                roleArn: `arn:aws:iam::${config.account}:role/YourDeploymentRole`,
+            },
+        });
+
+        // Deploy resources in the tenant's account using this provider
+        const bucket = new aws.s3.Bucket(`tenant-${config.account}-bucket`, {}, { provider });
+    });
+});
